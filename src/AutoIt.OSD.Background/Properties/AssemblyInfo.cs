d2a92427bf88f7d285c318fa3aedc8270509faed
Jonathan Bennett <jon@autoitscript.com>
2018-06-26 15:08:09 +0100

    Updated version.

--- src/AutoIt.OSD.Background/Properties/AssemblyInfo.cs
+++ src/AutoIt.OSD.Background/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoIt.OSD.Background")]
-[assembly: AssemblyDescription("Shows the background during an MDT/SCCM OSD")]
+[assembly: AssemblyDescription("Custom background and debug tools for SCCM OSD")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AutoIt Consulting Ltd")]
 [assembly: AssemblyProduct("AutoIt.OSD.Background")]
-[assembly: AssemblyCopyright("Copyright ©2018 AutoIt Consulting")]
+[assembly: AssemblyCopyright("Copyright ©2018 AutoIt Consulting Ltd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
